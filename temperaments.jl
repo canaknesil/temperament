d1e9eeb404c1a18d58a9acdd0d09573afcb94b9a
Can Aknesil <aknesil@kth.se>
2023-07-05 16:35:15 +0200

    notes, scalets, and more scales

--- temperaments.jl
+++ temperaments.jl
@@ -1,4 +1,5 @@
 import PyPlot as plt
+import OrderedCollections: OrderedDict as Dict
 
 
 n_harmonics = 31 # 31
@@ -26,7 +27,7 @@ function plot_dots_at_height(annotations::AbstractVector, data::AbstractVector, 
     push!(y_label_locations, height)
     push!(y_labels, label)
     
-    plt.plot(data, fill(height, length(data)), "*")
+    plt.plot(data, fill(height, length(data)), "*", markersize=8)
     for i = 1:length(data)
         plt.annotate(annotations[i], (data[i], height))
     end
@@ -36,6 +37,17 @@ function to_dict(v::AbstractVector)
     d = Dict()
     for i = 1:length(v)
         d[i] = v[i]
+    end
+    return d
+end
+
+scale_to_cents(scale::AbstractVector, temperament_map::AbstractVector) =
+    map(i -> temperament_map[i], scale)
+
+function scale_to_cents(scales::AbstractDict, temperament_map::AbstractVector)
+    d = Dict()
+    for name in keys(scales)
+        d[name] = scale_to_cents(scales[name], temperament_map)
     end
     return d
 end
@@ -58,6 +70,7 @@ for k in keys(harmonics)
     harmonics[k] = to_cents(h)
 end
 
+
 # Define possible fundamental frequencies for the reference note.
 fundamentals = copy(harmonics)
 delete!(fundamentals, 1)
@@ -75,6 +88,14 @@ if length(eq_temp_53) == 53
 end
 @assert(length(eq_temp_53) == 54)
 
+western_notes_names = ["do", "", "re", "", "mi", "fa", "", "sol", "", "la", "", "si", "do"]
+western_notes = eq_temp_12
+
+turkish_classical_note_names =
+    ["do", "", "", "", "re", "", "", "", "mi", "fa", "", "", "", "", "sol", "", "", "", "la", "", "", "", "si", "", "do"]
+turkish_classical_notes =
+    map(x -> eq_temp_53[x+1], [0 , 4 , 5 , 8 ,  9 , 13, 14, 17, 18, 22, 23, 26, 27, 30, 31, 35, 36, 39, 40, 44, 45, 48, 49, 52, 53])
+
 
 
 # Define Western scales
@@ -82,35 +103,44 @@ end
 DO, RE, MI, FA, SOL, LA, SI, DO_2 = [1, 3, 5, 6, 8, 10, 12, 13]
 
 western_scales = Dict(
-    "Ionian" => [DO, RE, MI, FA, SOL, LA, SI, DO_2]
+    "Ionian (I)"     => [DO  , RE  , MI  , FA  , SOL  , LA  , SI  , DO_2],
+    "Dorian (II)"    => [DO  , RE  , MI-1, FA  , SOL  , LA  , SI-1, DO_2],
+    "Phrygian (III)" => [DO  , RE-1, MI-1, FA  , SOL  , LA-1, SI-1, DO_2],
+    "Lydian (IV)"    => [DO  , RE  , MI  , FA+1, SOL  , LA  , SI  , DO_2],
+    "Mixolydian (V)" => [DO  , RE  , MI  , FA  , SOL  , LA  , SI-1, DO_2],
+    "Aeolian (VI)"   => [DO  , RE  , MI-1, FA  , SOL  , LA-1, SI-1, DO_2],
+    "Locrian (VII)"  => [DO  , RE-1, MI-1, FA  , SOL-1, LA-1, SI-1, DO_2]
 )
 
-d = western_scales
-western_scales = Dict()
-for scale in keys(d)
-    western_scales[scale] = map(i -> eq_temp_12[i], d[scale])
-end
 
 # Define Turkish scales
 DO, RE, MI, FA, SOL, LA, SI, DO_2 = [1, 10, 19, 23, 32, 41, 50, 54]
 
-turkish_scales = Dict(
-    "Çagah dörtlüsü/beşlisi" => [DO, RE, MI, FA, SOL],
-    "Buselik dörtlüsü/beşlisi" => [DO, RE, MI-5, FA, SOL],
-    "Kürdi dörtlüsü/beşlisi" => [DO, RE-5, MI-5, FA, SOL],
-    "Rast dörtlüsü/beşlisi" => [DO, RE, MI-1, FA, SOL],
-    "Hicaz dörtlüsü/beşlisi" => [DO, RE-4, MI-1, FA, SOL],
-    "Uşşak dörtlüsü" => [DO, RE-1, MI-5, FA],
-    
+# Tetrachords (dörtlüler) and pentachords (beşliler)
+turkish_classical_scalets = Dict(
+    "Çagah dörtlüsü"   => [DO  , RE  , MI  , FA],
+    "Çagah beşlisi"    => [DO  , RE  , MI  , FA  , SOL],
+    "Buselik dörtlüsü" => [DO  , RE  , MI-5, FA],
+    "Buselik beşlisi"  => [DO  , RE  , MI-5, FA  , SOL],
+    "Kürdi dörtlüsü"   => [DO  , RE-5, MI-5, FA],
+    "Kürdi beşlisi"    => [DO  , RE-5, MI-5, FA  , SOL],
+    "Rast dörtlüsü"    => [DO  , RE  , MI-1, FA],
+    "Rast beşlisi"     => [DO  , RE  , MI-1, FA  , SOL],
+    "Hicaz dörtlüsü"   => [DO  , RE-4, MI-1, FA],
+    "Hicaz beşlisi"    => [DO  , RE-4, MI-1, FA  , SOL],
+    "Uşşak dörtlüsü"   => [DO  , RE-1, MI-5, FA],
+    "Hüseyni beşlisi"  => [DO  , RE-1, MI-5, FA  , SOL]
 )
 
-d = turkish_scales
-turkish_scales = Dict()
-for scale in keys(d)
-    turkish_scales[scale] = map(i -> eq_temp_53[i], d[scale])
-end
+turkish_classical_scales = Dict(
+    "Çargah makamı"  => [DO  , RE  , MI  , FA  , SOL  , LA  , SI  , DO_2]
+)
 
-scales = merge(western_scales, turkish_scales)
+
+western_scales = scale_to_cents(western_scales, eq_temp_12)
+turkish_classical_scalets = scale_to_cents(turkish_classical_scalets, eq_temp_53)
+turkish_classical_scales = scale_to_cents(turkish_classical_scales, eq_temp_53)
+
 
 
 # Plot scales wrt intervals
@@ -119,28 +149,48 @@ height = 0
 plot_dots_at_height(harmonics, height, "Harmonics")
 plot_dots_at_height(fundamentals, height -= 0.5, "Fundamentals")
 plot_dots_at_height(eq_temp_12, height -= 0.5, "Equal temp. 12")
+plot_dots_at_height(western_notes_names,
+                    western_notes, height -= 0.5, "Wetern Music Notes")
 plot_dots_at_height(eq_temp_53, height -= 0.5, "Equal temp. 53")
+plot_dots_at_height(turkish_classical_note_names,
+                    turkish_classical_notes, height -= 0.5, "Turkish Classical Music Notes")
 
 height -= 0.5
-function plot_scale(scale)
+function plot_scales(scales)
     global height
-    plot_dots_at_height(scales[scale], height -= 0.5, scale)
+    for name in keys(scales)
+        plot_dots_at_height(scales[name], height -= 0.5, name)
+    end
 end
 
-for scale in keys(western_scales)
-    plot_scale(scale)
+plot_scales(western_scales)
+height -= 0.5
+plot_scales(turkish_classical_scalets)
+height -= 0.5
+plot_scales(turkish_classical_scales)
+
+#plt.vlines(collect(values(harmonics)), height, 0, alpha=0.5)
+#plt.vlines(collect(values(fundamentals)), height, 0, color="orange", alpha=0.5)
+
+# after nth harmonic, fade the lines
+n = 3
+for k in keys(harmonics)
+    if k >= n
+        alpha = 1 / (k - n + 1)
+    else
+        alpha = 1
+    end
+    alpha = sqrt(alpha) # slow down the fading
+    plt.vlines(harmonics[k], height, 0, alpha=alpha)
+    if k > 2
+        plt.vlines(fundamentals[k], height, 0, alpha=alpha, color="orange")
+    end
 end
 
-for scale in keys(turkish_scales)
-    plot_scale(scale)
-end
-
-plt.vlines(collect(values(harmonics)), height, 0)
-plt.vlines(collect(values(fundamentals)), height, 0, color="orange")
 
 plt.yticks(y_label_locations, y_labels)
 plt.xlabel("cents")
-
+plt.tight_layout()
 plt.show()
 
 
